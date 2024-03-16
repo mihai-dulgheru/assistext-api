dcb8bbd952c1f42d94146c061c697ffc6dcdc52a
Mihai-Nicolae Dulgheru <dulgherumihai19@stud.ase.ro>
2024-03-16 05:14:16 +0200

    Refactor completions route and add mock response

--- server/routes/completions.js
+++ server/routes/completions.js
@@ -1,51 +1,52 @@
+// import axios from 'axios';
 import { Router } from 'express';
 const router = Router();
 
 /* POST completions */
-router.post('/', async function (_req, res) {
-  try {
-    // const response = await axios.post(
-    //   'https://api.openai.com/v1/completions',
-    //   {
-    //     model: 'gpt-3.5-turbo-instruct',
-    //     prompt: req.body.prompt,
-    //     max_tokens: req.body.maxTokens,
-    //     temperature: req.body.temperature,
-    //   },
-    //   {
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //       Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-    //     },
-    //   }
-    // );
-
-    // res.json(response.data);
-
-    // Mock response
-    res.json({
-      id: 'cmpl-93AWrOqYgUFttse0oqAeiXHBk54He',
-      object: 'text_completion',
-      created: 1710542485,
-      model: 'gpt-3.5-turbo-instruct',
-      choices: [
-        {
-          text: ' 180°\n\nAceasta este o',
-          index: 0,
-          logprobs: null,
-          finish_reason: 'length',
-        },
-      ],
-      usage: {
-        prompt_tokens: 13,
-        completion_tokens: 8,
-        total_tokens: 21,
+router.post('/', async function (req, res) {
+  // Mock response
+  res.json({
+    id: 'cmpl-93AWrOqYgUFttse0oqAeiXHBk54He',
+    object: 'text_completion',
+    created: 1710542485,
+    model: 'gpt-3.5-turbo-instruct',
+    choices: [
+      {
+        text: ' 180°\n\nAceasta este o',
+        index: 0,
+        logprobs: null,
+        finish_reason: 'length',
       },
-    });
-  } catch (error) {
-    console.log(error.toJSON());
-    res.status(500).json({ name: error.name, message: error.message });
-  }
+    ],
+    usage: {
+      prompt_tokens: 13,
+      completion_tokens: 8,
+      total_tokens: 21,
+    },
+  });
+
+  // try {
+  //   const response = await axios.post(
+  //     'https://api.openai.com/v1/completions',
+  //     {
+  //       model: 'gpt-3.5-turbo-instruct',
+  //       prompt: req.body.prompt,
+  //       max_tokens: req.body.maxTokens,
+  //       temperature: req.body.temperature,
+  //     },
+  //     {
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+  //       },
+  //     }
+  //   );
+
+  //   res.json(response.data);
+  // } catch (error) {
+  //   console.log(error.toJSON());
+  //   res.status(500).json({ name: error.name, message: error.message });
+  // }
 });
 
 export default router;
